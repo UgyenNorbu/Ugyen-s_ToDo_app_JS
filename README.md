727bd565cfa79cbcc417b9f49b4acebff029e72a
Ugyen Norbu <66914661+UgyenNorbu@users.noreply.github.com>
2021-12-11 14:07:19 +0600

    commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Ugyen-s_ToDo_app_JS
+
+This project is made as an exercise file to the tutorial "Beginner Vanilla Javascript Project Tutorial" by Dav Ed available on https://www.youtube.com/watch?v=Ttf3CEsEwMQ
--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 //Selectors
-const todoInput = document.querySelector('.todo-input')
-const todoButton = document.querySelector('.todo-button')
-const todoList = document.querySelector('.todo-list')
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
 
 //Event listenrs
+todoButton.addEventListener('click', addTodo);
 
-//Functions test
+//Functions
+function addTodo(event) {
+    // Prevent form from submitting
+    event.preventDefault();
+
+    // Create Todo Div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    // Create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo)
+
+    // Complete button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class = "fas fa-check"></i>';
+    completedButton.classList.add('completed-btn');
+    todoDiv.appendChild(completedButton);
+
+    // Delete button
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = '<i class = "fas fa-trash"></i>';
+    deleteButton.classList.add('delete-btn');
+    todoDiv.appendChild(deleteButton);
+
+    // Append the todoDiv to the 'ul' list
+    todoList.appendChild(todoDiv);
+
+    // Clear todoInput value of the input form
+    todoInput.value = "";
+}
